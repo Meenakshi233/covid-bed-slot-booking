b8582dfe753404d9a69195eee30d96ab4412def0
Meenakshi233 <batchufisheye@gmail.com>
2024-06-15 12:12:28 +0530

    added main.py file

---
+++ covid-bed-slot-booking/backend/main.py
@@ -0,0 +1,37 @@
+from flask import Flask,redirect,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# mydatabase connection
+local_server = True
+app = Flask(__name__)
+app.secret_key = "hemmee"
+
+#app.config['SQLALCHEMY_DATABASE_URI']= "mysql://username:password@localhost/databasename"
+app.config['SQLALCHEMY_DATABASE_URI']= "mysql://root:@localhost/SARS_Cov_2"
+db=SQLAlchemy(app)
+
+class Test(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(50))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/usersignup")
+def usersignup():
+    return render_template("usersignup.html")
+
+#testing whether db is connected or not
+@app.route("/test")
+def test():
+    try:
+        a=Test.query.all()
+        print(a)
+        return "My database is connected"
+    except Exception as e:
+        print(e)
+        return "My database is not connected"
+    
+app.run(debug=True)
