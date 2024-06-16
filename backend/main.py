37537bc5e3fdfe6ec23b87e1583df772e00801cb
Hema Chandrika Hari <harihemachandrika@gmail.com>
2024-06-16 13:21:38 +0530

    changes done

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,180 @@
-from flask import Flask,redirect,render_template
+from flask import Flask, json,redirect,render_template,flash,request
+from flask.globals import request, session
+from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+from flask_login import login_required,logout_user,login_user,login_manager,LoginManager,current_user
+
+
+import json
 
 # mydatabase connection
 local_server = True
 app = Flask(__name__)
 app.secret_key = "hemmee"
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  
+
+
 #app.config['SQLALCHEMY_DATABASE_URI']= "mysql://username:password@localhost/databasename"
-app.config['SQLALCHEMY_DATABASE_URI']= "mysql://root:@localhost/SARS_Cov_2"
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/covid'
 db=SQLAlchemy(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Test(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(50))
+
+class User(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    srfid=db.Column(db.String(20),unique=True)
+    email=db.Column(db.String(50))
+    dob=db.Column(db.String(1000))
+    
+class Hospitaluser(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    hcode=db.Column(db.String(20))
+    email=db.Column(db.String(50))
+    password=db.Column(db.String(1000))
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/usersignup")
-def usersignup():
+
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        email=request.form.get('email')
+        dob=request.form.get('dob')
+        # print(srfid,email,dob)
+        #encpassword=generate_password_hash(dob)
+        user=User.query.filter_by(srfid=srfid).first()
+        emailUser=User.query.filter_by(email=email).first()
+        if user or emailUser:
+            flash("Email or srif is already taken","warning")
+            return render_template("usersignup.html")
+        # new_user=db.engine.execute(f"INSERT INTO `user` (`srfid`,`email`,`dob`) VALUES ('{srfid}','{email}','{dob}') ")
+        new_user=User(srfid=srfid,email=email,dob=dob)
+        db.session.add(new_user)
+        db.session.commit()
+                
+        flash("SignUp Success Please Login","success")
+        return render_template("userlogin.html")
+
     return render_template("usersignup.html")
 
-@app.route("/userlogin")
-def userlogin():
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=="POST":
+        srfid=request.form.get('srf')
+        dob=request.form.get('dob')
+        user=User.query.filter_by(srfid=srfid).first()
+        if user and user.dob==dob:
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("userlogin.html")
+
+
     return render_template("userlogin.html")
 
-#testing whether db is connected or not
+@app.route('/hospitallogin',methods=['POST','GET'])
+def hospitallogin():
+    if request.method=="POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=Hospitaluser.query.filter_by(email=email).first()
+        #if user and check_password_hash(user.password,password):
+        if user and user.password==password:
+            login_user(user)
+            flash("Login Success","info")
+            return render_template("index.html")
+        else:
+            flash("Invalid Credentials","danger")
+            return render_template("hospitallogin.html")
+
+@app.route('/admin',methods=['POST','GET'])
+def admin():
+ 
+    if request.method=="POST":
+        username=request.form.get('username')
+        password=request.form.get('password')
+        if(username=="admin" and password=="admin"):
+            session['user']=username
+            flash("login success","info")
+            return render_template("addHosUser.html")
+        else:
+            flash("Invalid Credentials","danger")
+
+    return render_template("admin.html")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logout SuccessFul","warning")
+    return redirect(url_for('login'))
+
+@app.route('/addHospitalUser',methods=['POST','GET'])
+def hospitalUser():
+   
+    if('user' in session and session['user']=="admin"):
+      
+        if request.method=="POST":
+            hcode=request.form.get('hcode')
+            email=request.form.get('email')
+            password=request.form.get('password')        
+           
+            hcode=hcode.upper()      
+            emailUser=Hospitaluser.query.filter_by(email=email).first()
+            if  emailUser:
+                flash("Email or srif is already taken","warning")
+         
+           
+            query=Hospitaluser(hcode=hcode,email=email,password=password)
+            db.session.add(query)
+            db.session.commit()
+
+           
+
+            flash("Data Sent and Inserted Successfully","warning")
+            return render_template("addHosUser.html")
+
+    else:
+        flash("Login and try Again","warning")
+        return render_template("addHosUser.html")
+    
+    
+    
 @app.route("/test")
 def test():
     try:
-        a=Test.query.all()
+        a = Test.query.all()
         print(a)
         return "My database is connected"
     except Exception as e:
         print(e)
         return "My database is not connected"
     
-app.run(debug=True)
+@app.route("/logoutadmin")
+def logoutadmin():
+    session.pop('user')
+    flash("You are logout admin", "primary")
+
+    return redirect('/admin')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
